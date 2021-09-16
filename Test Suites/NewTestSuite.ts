e590a3ea742b40b5f0d9453338c014bd56b6a950
Russo Luca <RussoLu@valueteam.com>
2021-09-16 12:46:18 +0200

    first init

---
+++ Test Suites/NewTestSuite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>NewTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6f8a5b41-344e-4ce1-ad44-38409df980d8</testSuiteGuid>
+   <testCaseLink>
+      <guid>95fa0fc7-63f4-4362-8ccd-37c1339a51eb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Pluto</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
